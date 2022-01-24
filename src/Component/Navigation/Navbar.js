b7062e671d3cc77e291ee487ff214600f6d709c9
Murad Ahmed <muradahmed199771@gmail.com>
2022-01-24 18:21:13 +0600

    nav bar done

---
+++ src/Component/Navigation/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../../assets/Shape 1 3.svg'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                    <a className="navbar-brand" href="#"> <img src={logo} alt="" width="129" height="32"/></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse " id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="#">Products</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
