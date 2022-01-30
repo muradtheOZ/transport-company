8cb38f114ea406a19215f210c87503d32f20092a
Murad Ahmed <muradahmed199771@gmail.com>
2022-01-31 00:45:45 +0600

    design changed for backbone section

--- src/Component/Backbone/Backbone.js
+++ src/Component/Backbone/Backbone.js
@@ -6,9 +6,9 @@ const Backbone = () => {
     return (
         <div className="backbone">
             <div className="container">
-                <div className="row ">
-                    <div className="col-md-6">
-                        <div className="container mt-5 pt-5 mb-5 pb-3">
+                <div className="row">
+                    <div className="col-md-6 pt-5">
+                        <div className="container mt-5 pt-5 pb-3">
                             <h2 className="mt-5 pt-5">The Backbone for Internet Business Worldwide</h2>
                             <h4 className="mt-4 pt-4 mb-5">Add di!erent types of vehicles in the same app easily. Started with scooters, but want to add bikes? No problem! We’ll help you</h4>
                             <a className="btn btn-lg mt-5" href="#" role="button">get Started</a>
